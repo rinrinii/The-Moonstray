bc04249cc6a9be0b79c6cd0948383af95c3415f3
Marien <skyyemarin@gmail.com>
2026-04-30 17:51:45 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSmoothTime = 0.1f;
+    public float gravity = -9.81f;
+    public float sprintMultiplier = 1.5f;
+
+    private CharacterController controller;
+    private PlayerTransformation transformation;
+    private Animator currentAnim;
+    private Transform cam;
+    private Vector3 velocity;
+    private float turnSmoothVelocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        transformation = GetComponent<PlayerTransformation>();
+        cam = Camera.main.transform;
+        UpdateAnimator();
+    }
+
+    public void UpdateAnimator()
+    {
+        currentAnim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (!transformation.CanMove())
+        {
+            if (currentAnim != null)
+            {
+                currentAnim.SetFloat("Speed", 0, 0.15f, Time.deltaTime);
+            }
+
+            ApplyGravity();
+            return;
+        }
+
+        float baseSpeed = transformation.GetSpeed();
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift);
+        float currentSpeed = isSprinting ? baseSpeed * sprintMultiplier : baseSpeed;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(x, 0f, z).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir * currentSpeed * Time.deltaTime);
+        }
+
+        if (currentAnim != null)
+        {
+            float speedPercent = direction.magnitude * currentSpeed;
+            currentAnim.SetFloat("Speed", speedPercent, 0.15f, Time.deltaTime);
+        }
+
+        ApplyGravity();
+    }
+
+    void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerTransformation.cs
@@ -0,0 +1,156 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+
+public class PlayerTransformation : MonoBehaviour
+{
+    public enum FormState { Human, Wolf }
+    public FormState currentForm = FormState.Human;
+
+    [Header("Models")]
+    public GameObject humanModel;
+    public GameObject wolfModel;
+
+    [Header("Movement")]
+    public float humanSpeed = 5f;
+    public float wolfSpeed = 9f;
+
+    [Header("Collider Settings")]
+    public float humanHeight = 1.8f;
+    public float wolfHeight = 1.0f;
+
+    [Header("World Effects")]
+    public Light sunLight;
+    public Vector2 dayRotation = new Vector2(120f, 150f);
+    public Vector2 nightRotation = new Vector2(356f, 330f);
+    public Volume dayVol;
+    public Volume nightVol;
+    public float transitionDuration = 1.5f;
+
+    [Header("VFX")]
+    public ParticleSystem transformParticles;
+    public Light transformLight;
+    public GameObject glowSphere;
+    public float maxLightIntensity = 20f;
+    public float maxSphereScale = 3f;
+
+    private float currentSpeed;
+    private CharacterController controller;
+    private bool isTransitioning = false;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        ApplyHumanForm();
+        
+        if(dayVol) dayVol.weight = 1f;
+        if(nightVol) nightVol.weight = 0f;
+        if(transformLight) transformLight.intensity = 0f;
+        if(glowSphere) glowSphere.SetActive(false);
+        
+        if(sunLight)
+            sunLight.transform.rotation = Quaternion.Euler(dayRotation.x, dayRotation.y, 0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && !isTransitioning)
+        {
+            StartCoroutine(TransformationSequence());
+        }
+    }
+
+    public bool CanMove()
+    {
+        return !isTransitioning;
+    }
+
+    IEnumerator TransformationSequence()
+    {
+        isTransitioning = true;
+        bool turningToWolf = (currentForm == FormState.Human);
+        
+        float time = 0;
+        Quaternion startRot = sunLight.transform.rotation;
+        Vector2 targetV2 = turningToWolf ? nightRotation : dayRotation;
+        Quaternion endRot = Quaternion.Euler(targetV2.x, targetV2.y, 0f);
+
+        float startDayWeight = dayVol.weight;
+        float startNightWeight = nightVol.weight;
+
+        while (time < transitionDuration)
+        {
+            float t = time / transitionDuration;
+            
+            if (sunLight)
+                sunLight.transform.rotation = Quaternion.Slerp(startRot, endRot, t);
+            
+            dayVol.weight = Mathf.Lerp(startDayWeight, turningToWolf ? 0f : 1f, t);
+            nightVol.weight = Mathf.Lerp(startNightWeight, turningToWolf ? 1f : 0f, t);
+
+            // Sharp Burst Logic
+            float burstCurve = Mathf.Pow(Mathf.Sin(t * Mathf.PI), 10); 
+            
+            if (transformLight)
+                transformLight.intensity = burstCurve * maxLightIntensity;
+
+            if (glowSphere)
+            {
+                glowSphere.SetActive(true);
+                glowSphere.transform.localScale = Vector3.one * (burstCurve * maxSphereScale);
+                Material glowMat = glowSphere.GetComponent<MeshRenderer>().material;
+                glowMat.SetColor("_EmissionColor", Color.white * (burstCurve * 50f));
+            }
+
+            if (time >= transitionDuration / 2f)
+            {
+                if (turningToWolf && currentForm == FormState.Human)
+                {
+                    ApplyWolfForm();
+                    if (transformParticles) transformParticles.Play();
+                    GetComponent<PlayerMovement>().UpdateAnimator();
+                }
+                else if (!turningToWolf && currentForm == FormState.Wolf)
+                {
+                    ApplyHumanForm();
+                    if (transformParticles) transformParticles.Play();
+                    GetComponent<PlayerMovement>().UpdateAnimator();
+                }
+            }
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        if (sunLight) sunLight.transform.rotation = endRot;
+        if (transformLight) transformLight.intensity = 0f;
+        if (glowSphere) glowSphere.SetActive(false);
+
+        isTransitioning = false;
+    }
+
+    void ApplyHumanForm()
+    {
+        currentForm = FormState.Human;
+        humanModel.SetActive(true);
+        wolfModel.SetActive(false);
+        currentSpeed = humanSpeed;
+        controller.height = humanHeight;
+        controller.center = new Vector3(0, humanHeight / 2f, 0);
+    }
+
+    void ApplyWolfForm()
+    {
+        currentForm = FormState.Wolf;
+        humanModel.SetActive(false);
+        wolfModel.SetActive(true);
+        currentSpeed = wolfSpeed;
+        controller.height = wolfHeight;
+        controller.center = new Vector3(0, wolfHeight / 2f, 0);
+    }
+
+    public float GetSpeed()
+    {
+        return currentSpeed;
+    }
+}
